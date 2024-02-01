9b2856d6d83188bc4e30c2ab5bfc261f6528513a
Jumvi <judahmvi@gmail.com>
2024-02-01 21:16:28 +0100

    refactor:modification of imput retrieval functionality using useForm

--- src/components/TweetEditoir.jsx
+++ src/components/TweetEditoir.jsx
@@ -11,22 +11,23 @@ import { addInput } from '../feature/tweetSlicer';
 import coeur from '../images/coeur.png';
 import verified from '../images/Verified.png';
 import { incrementContext } from '../feature/tweetSlicer';
- 
+import { useForm } from 'react-hook-form';
+
 function TweetEditor(){
-    const [textInput,setTextInput] = useState("");
+    let [textInput,setTextInput] = useState("");
     const dispatch = useDispatch();
-    const [imageTweet,setImageTweet]=useState(null)
-    const contextNumber = useSelector((state)=>state.context)
+    let [imageTweet,setImageTweet]=useState(null)
+    const contextNumber = useSelector((state)=>state.context);
+
+    const {
+        register,
+        handleSubmit,
+        formState:{errors},
+    } = useForm()
     
-    const handlChange = (e)=>{
-        if(e.target.type === "text"){
-            setTextInput(e.target.value);
-         }else if (e.target.type=== 'file' && e.target.files.length > 0 ) { 
-            setImageTweet(e.target.files[0]);
-      
-        }
-        }
-    const handelClick = ()=>{
+    const handelClick = (data)=>{
+        setImageTweet(imageTweet =data.myFile);
+        setTextInput(textInput= data.myInput);
         if(imageTweet){
             dispatch(addInput({text:textInput,
                 src:profil,
@@ -34,14 +35,13 @@ function TweetEditor(){
                 like:coeur,
                 pseudo:'@Jmvi',
                 verifiedIcon:verified,
-                imgContent:URL.createObjectURL(imageTweet),//ceci permet de ne recupéré que l'url du file et non toutes les données.
+                imgContent:data.myFile,//ceci permet de ne recupéré que l'url du file et non toutes les données.
                 isCert:true,
                 // comment: <FaRegComment />,
                 // share:<IoShareOutline />,
                 // bookmark:<CiBookmark />
     
                 })); 
-            //  setNumberOfTWeet(numberOtweet +1)
              dispatch(incrementContext(contextNumber +1))
         }else if (textInput ){
             dispatch(addInput({text:textInput,
@@ -59,14 +59,14 @@ function TweetEditor(){
                 console.log(contextNumber)
                 dispatch(incrementContext(contextNumber +1))
                 console.log(`modi ${contextNumber +1}`);
-        }else{
-            return false
-        }
-           
+        }   
         setTextInput("");
         setImageTweet(null)
-        // a
        
+    }
+
+    const onSubmit = (data)=>{
+        handelClick(data);
     }
     useEffect(()=>{
         
@@ -77,29 +77,30 @@ function TweetEditor(){
             <div >
                 <Avatar src={profil} />
             </div>
-            <div className='tweet-editor-form  flex-auto'>
-                 <div >
-                    <input type="text" value={textInput} className='tweet-editor-input h-10 min-w-full border-0 outline-none text-xl bg-black text-white resize-none px-2 mx-2' placeholder='What s happenning ' onChange={handlChange}/>
-                </div>
-                <div className='tweet-editor-buttons flex itemes-center justify-between'>
-                    <div className='tweet-editor-actions flex items-center flex-start' >
-                    <label htmlFor="image-input">
-                        <input type="file" accept="image/jpeg,image/png" id='image-input'  onChange={handlChange}/>
-                        <img src={photo} alt="" />    
-                    </label>
-                    {/* <label htmlFor="image-input">
-                        <img src={gif} alt="" />
-                    </label> */}
+            <form action="post" onSubmit={handleSubmit(data =>onSubmit(data))}>
+                <div className='tweet-editor-form  flex-auto'>
+                    <input type="text" className='tweet-editor-input h-10 min-w-full border-0 outline-none text-xl bg-black text-white resize-none px-2 mx-2' placeholder='What s happenning ' {...register('myInput',{required:true})}/>
+                    {errors.myInput && <h4> ce champs est obligatoire</h4>}
+                    <div className='tweet-editor-buttons flex itemes-center justify-between'>
+                        <div className='tweet-editor-actions flex items-center flex-start' >
+                            <label htmlFor="image-input">
+                                <input type="file" accept="image/jpeg,image/png,image/*" id='image-input'  {...register('myFile',{required:false})}/>
+                                <img src={photo} alt="" />    
+                            </label>
+                            {/* <label htmlFor="image-input">
+                                 <img src={gif} alt="" />
+                                </label> */}
                         
-                        <img src= {poll} alt="" />
-                        <img src= {calendar} alt="" />
+                            <img src= {poll} alt="" />
+                            <img src= {calendar} alt="" />
+                        </div>
+                        <input type="submit" className='button bg-indigo-700 capitalize p-2 w-24 rounded border-none' placeholder='Tweet' />
                     </div>
-                    <div>
-                        <button className='button bg-indigo-700 capitalize p-2 w-24 rounded border-none' onClick={handelClick} >Tweet</button>
-                    </div>
-                </div>
             
-            </div>
+                </div>
+
+            </form>
+            
         </div>
     )
   
