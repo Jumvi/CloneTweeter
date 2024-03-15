beed962a361df24741161d49358b8339e26518e2
Jumvi <judahmvi@gmail.com>
2024-03-15 17:26:41 +0100

    feat:manage clicks on tweet icons

--- src/components/tweetAction.jsx
+++ src/components/tweetAction.jsx
@@ -1,36 +1,33 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getId } from '../feature/tweetSlicer';
-import {removeTweet} from "../feature/tweetSlicer";
+import { removeTweet } from '../feature/tweetSlicer';
 
-
-
-
-function TweetAction({icon,counter,id}) {
-
-    const [isLiked,setIsLiked] = useState(true);
+function TweetAction({ icon, counter, id }) {
+    const [isLiked, setIsLiked] = useState(true);
     const dispatch = useDispatch();
 
-    function onClick(e){
-        id = e.target.id;
-        
-        if(isLiked && id){
-            counter +1;
+    function onClick(e) {
+        if (isLiked && id) {
             setIsLiked(false);
-            dispatch(getId(id))
-        }else{
-            counter -1;
+            dispatch(getId(id));
+            console.log('jai cliqué', isLiked,{id});
+
+        } else {
             setIsLiked(true);
             dispatch(removeTweet(id));
+            console.log('je click encore', isLiked);
         }
     }
 
+    isLiked? counter = counter +1 -1 : counter  = counter-1;
+
     return (
         <div>
-            <div className='flex gap-1 text-white'>
-                <span className='cursor-pointer' onClick={onClick} id= {id} >{icon}</span>
+            <div className='flex gap-1 text-white' onClick={onClick} id={id}>
+                <span className='cursor-pointer'>{icon}</span>
                 <span>{counter}</span>
-            </div>   
+            </div>
         </div>
     );
 }
